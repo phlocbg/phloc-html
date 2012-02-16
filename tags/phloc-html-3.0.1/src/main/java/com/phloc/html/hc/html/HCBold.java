df168e24cf9d4c4dcaad5a599bf44d6583399b27
ph@phloc.com <ph@phloc.com@bfa6b85e-8048-6ac0-6266-db734edb6865>
2012-02-16 20:47:34 +0000

    [maven-release-plugin]  copy for tag phloc-html-3.0.1

---
+++ tags/phloc-html-3.0.1/src/main/java/com/phloc/html/hc/html/HCBold.java
@@ -0,0 +1,71 @@
+/**
+ * Copyright (C) 2006-2012 phloc systems
+ * http://www.phloc.com
+ * office[at]phloc[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.phloc.html.hc.html;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import com.phloc.commons.text.IPredefinedLocaleTextProvider;
+import com.phloc.html.EHTMLElement;
+import com.phloc.html.hc.IHCNode;
+import com.phloc.html.hc.impl.AbstractHCElementWithChildren;
+
+/**
+ * HTML5: the specific semantic purpose of representing text “offset from its
+ * surrounding content without conveying any extra emphasis or importance, and
+ * for which the conventional typographic presentation is bold text”.
+ * 
+ * @author philip
+ */
+public final class HCBold extends AbstractHCElementWithChildren <HCBold>
+{
+  public HCBold ()
+  {
+    super (EHTMLElement.B);
+  }
+
+  public HCBold (@Nonnull final IPredefinedLocaleTextProvider aChild)
+  {
+    this (aChild.getText ());
+  }
+
+  public HCBold (@Nullable final String sChild)
+  {
+    super (EHTMLElement.B, sChild);
+  }
+
+  public HCBold (@Nullable final String... aChildren)
+  {
+    super (EHTMLElement.B, aChildren);
+  }
+
+  public HCBold (@Nullable final IHCNode aChild)
+  {
+    super (EHTMLElement.B, aChild);
+  }
+
+  public HCBold (@Nullable final IHCNode... aChildren)
+  {
+    super (EHTMLElement.B, aChildren);
+  }
+
+  public HCBold (@Nullable final Iterable <? extends IHCNode> aChildren)
+  {
+    super (EHTMLElement.B, aChildren);
+  }
+}
---
+++ tags/phloc-html-3.0.1/src/main/java/com/phloc/html/hc/html/HCDeleted.java
@@ -0,0 +1,64 @@
+/**
+ * Copyright (C) 2006-2012 phloc systems
+ * http://www.phloc.com
+ * office[at]phloc[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.phloc.html.hc.html;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import com.phloc.commons.text.IPredefinedLocaleTextProvider;
+import com.phloc.html.EHTMLElement;
+import com.phloc.html.hc.IHCNode;
+import com.phloc.html.hc.impl.AbstractHCElementWithChildren;
+
+public final class HCDeleted extends AbstractHCElementWithChildren <HCDeleted>
+{
+  public HCDeleted ()
+  {
+    super (EHTMLElement.DEL);
+  }
+
+  public HCDeleted (@Nonnull final IPredefinedLocaleTextProvider aChild)
+  {
+    this (aChild.getText ());
+  }
+
+  public HCDeleted (@Nullable final String sChild)
+  {
+    super (EHTMLElement.DEL, sChild);
+  }
+
+  public HCDeleted (@Nullable final String... aChildren)
+  {
+    super (EHTMLElement.DEL, aChildren);
+  }
+
+  public HCDeleted (@Nullable final IHCNode aChild)
+  {
+    super (EHTMLElement.DEL, aChild);
+  }
+
+  public HCDeleted (@Nullable final IHCNode... aChildren)
+  {
+    super (EHTMLElement.DEL, aChildren);
+  }
+
+  public HCDeleted (@Nullable final Iterable <? extends IHCNode> aChildren)
+  {
+    super (EHTMLElement.DEL, aChildren);
+  }
+}
