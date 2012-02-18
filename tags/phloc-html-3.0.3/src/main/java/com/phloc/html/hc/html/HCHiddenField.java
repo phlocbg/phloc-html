7a25e3dd9abde0992920a715d9b7e1815b983463
ph@phloc.com <ph@phloc.com@bfa6b85e-8048-6ac0-6266-db734edb6865>
2012-02-18 22:16:58 +0000

    [maven-release-plugin]  copy for tag phloc-html-3.0.3

---
+++ tags/phloc-html-3.0.3/src/main/java/com/phloc/html/hc/html/HCHiddenField.java
@@ -0,0 +1,98 @@
+/**
+ * Copyright (C) 2006-2012 phloc systems
+ * http://www.phloc.com
+ * office[at]phloc[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.phloc.html.hc.html;
+
+import java.util.Locale;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import com.phloc.commons.microdom.IMicroElement;
+import com.phloc.html.CHTMLAttributes;
+import com.phloc.html.hc.CHCParam;
+import com.phloc.html.hc.api.EHCInputType;
+import com.phloc.html.hc.conversion.HCConversionSettings;
+import com.phloc.html.hc.impl.AbstractHCInput;
+
+public class HCHiddenField extends AbstractHCInput <HCHiddenField>
+{
+  private String m_sValue;
+
+  public HCHiddenField ()
+  {
+    super (EHCInputType.HIDDEN);
+  }
+
+  public HCHiddenField (@Nullable final String sName, @Nullable final String sValue)
+  {
+    this ();
+    setName (sName);
+    setValue (sValue);
+  }
+
+  public HCHiddenField (@Nullable final String sID, @Nullable final String sName, @Nullable final String sValue)
+  {
+    this (sName, sValue);
+    setID (sID);
+  }
+
+  public HCHiddenField (@Nullable final String sName, final int nValue)
+  {
+    this (sName, Integer.toString (nValue));
+  }
+
+  public HCHiddenField (@Nullable final String sName, @Nonnull final Locale aLocale)
+  {
+    this (sName, aLocale.toString ());
+  }
+
+  public HCHiddenField (@Nullable final String sName, final boolean bValue)
+  {
+    this ();
+    setName (sName);
+    setValue (bValue);
+  }
+
+  @Nonnull
+  public final HCHiddenField setValue (final boolean bValue)
+  {
+    return setValue (bValue ? CHCParam.VALUE_CHECKED : CHCParam.VALUE_UNCHECKED);
+  }
+
+  @Nonnull
+  public final HCHiddenField setValue (@Nullable final String sValue)
+  {
+    m_sValue = sValue;
+    return this;
+  }
+
+  @Override
+  protected void applyProperties (final IMicroElement aElement, final HCConversionSettings aConversionSettings)
+  {
+    super.applyProperties (aElement, aConversionSettings);
+    if (m_sValue != null)
+      aElement.setAttribute (CHTMLAttributes.VALUE, m_sValue);
+  }
+
+  @Override
+  @Nonnull
+  public String getPlainText ()
+  {
+    return "";
+  }
+}
---
+++ tags/phloc-html-3.0.3/src/main/java/com/phloc/html/meta/MetaElement.java
@@ -0,0 +1,226 @@
+/**
+ * Copyright (C) 2006-2012 phloc systems
+ * http://www.phloc.com
+ * office[at]phloc[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.phloc.html.meta;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Set;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import javax.annotation.concurrent.NotThreadSafe;
+
+import com.phloc.commons.CGlobal;
+import com.phloc.commons.annotations.ReturnsImmutableObject;
+import com.phloc.commons.annotations.ReturnsMutableCopy;
+import com.phloc.commons.collections.ContainerHelper;
+import com.phloc.commons.compare.EqualsUtils;
+import com.phloc.commons.hash.HashCodeGenerator;
+import com.phloc.commons.state.EChange;
+import com.phloc.commons.string.ToStringGenerator;
+
+/**
+ * Represents a single HTML meta tag.
+ * 
+ * @author philip
+ */
+@NotThreadSafe
+public final class MetaElement implements IMetaElement
+{
+  public static final boolean DEFAULT_IS_HTTP_EQUIV = false;
+
+  /** tag name. */
+  private final String m_sName;
+
+  /** optional scheme string. */
+  private final String m_sScheme;
+
+  /** locale to value map. */
+  private final Map <Locale, String> m_aContents = new LinkedHashMap <Locale, String> ();
+
+  /** HTTP equivalent or not? */
+  private boolean m_bIsHttpEquiv;
+
+  public MetaElement (@Nonnull final String sName, @Nullable final String sScheme, final boolean bIsHttpEquiv)
+  {
+    if (sName == null)
+      throw new NullPointerException ("name");
+    m_sName = sName;
+    m_sScheme = sScheme;
+    m_bIsHttpEquiv = bIsHttpEquiv;
+  }
+
+  public MetaElement (@Nonnull final IMetaElementDeclaration aMetaTagDecl, @Nullable final String sContent)
+  {
+    this (aMetaTagDecl.getName (), aMetaTagDecl.getScheme (), aMetaTagDecl.isHttpEquiv (), null, sContent);
+  }
+
+  public MetaElement (@Nonnull final String sName, @Nullable final String sContent)
+  {
+    this (sName, null, DEFAULT_IS_HTTP_EQUIV, null, sContent);
+  }
+
+  public MetaElement (@Nonnull final String sName, final boolean bIsHttpEquiv, @Nullable final String sContent)
+  {
+    this (sName, null, bIsHttpEquiv, null, sContent);
+  }
+
+  public MetaElement (@Nonnull final String sName,
+                      @Nullable final String sScheme,
+                      final boolean bIsHttpEquiv,
+                      @Nullable final Locale aContentLocale,
+                      @Nullable final String sContent)
+  {
+    if (sName == null)
+      throw new NullPointerException ("name");
+    m_sName = sName;
+    m_sScheme = sScheme;
+    m_bIsHttpEquiv = bIsHttpEquiv;
+    setContent (aContentLocale, sContent);
+  }
+
+  @Nonnull
+  public String getName ()
+  {
+    return m_sName;
+  }
+
+  @Nullable
+  public String getScheme ()
+  {
+    return m_sScheme;
+  }
+
+  public boolean isHttpEquiv ()
+  {
+    return m_bIsHttpEquiv;
+  }
+
+  public boolean isLanguageIndependent ()
+  {
+    return m_aContents.containsKey (CGlobal.LOCALE_INDEPENDENT);
+  }
+
+  @Nonnull
+  @ReturnsImmutableObject
+  public Set <Locale> getAllLocales ()
+  {
+    return ContainerHelper.makeUnmodifiable (m_aContents.keySet ());
+  }
+
+  @Nonnull
+  @ReturnsImmutableObject
+  public Map <Locale, String> getContent ()
+  {
+    return ContainerHelper.makeUnmodifiable (m_aContents);
+  }
+
+  @Nonnull
+  private static Locale _getRealContentLocale (@Nullable final Locale aContentLocale)
+  {
+    return aContentLocale == null ? CGlobal.LOCALE_INDEPENDENT : aContentLocale;
+  }
+
+  @Nonnull
+  public EChange setContent (@Nullable final Locale aContentLocale, @Nullable final String sContent)
+  {
+    final Locale aRealContentLocale = _getRealContentLocale (aContentLocale);
+    final String sOldContent = m_aContents.get (aRealContentLocale);
+    if (EqualsUtils.nullSafeEquals (sOldContent, sContent))
+      return EChange.UNCHANGED;
+    if (sContent == null)
+      m_aContents.remove (aRealContentLocale);
+    else
+      m_aContents.put (aRealContentLocale, sContent);
+    return EChange.CHANGED;
+  }
+
+  @Nonnull
+  public EChange removeContent (@Nullable final Locale aContentLocale)
+  {
+    final Locale aRealContentLocale = _getRealContentLocale (aContentLocale);
+    // Manually check for key, as the value can be null
+    if (!m_aContents.containsKey (aRealContentLocale))
+      return EChange.UNCHANGED;
+    m_aContents.remove (aRealContentLocale);
+    return EChange.CHANGED;
+  }
+
+  @Nonnull
+  public EChange setHttpEquiv (final boolean bIsHttpEquiv)
+  {
+    if (m_bIsHttpEquiv == bIsHttpEquiv)
+      return EChange.UNCHANGED;
+    m_bIsHttpEquiv = bIsHttpEquiv;
+    return EChange.CHANGED;
+  }
+
+  @Nonnull
+  @ReturnsMutableCopy
+  public List <IMetaElementValue> getAsMetaElementValueList ()
+  {
+    final List <IMetaElementValue> ret = new ArrayList <IMetaElementValue> (m_aContents.size ());
+    for (final Map.Entry <Locale, String> aEntry : m_aContents.entrySet ())
+      ret.add (new MetaElementValue (m_sName, aEntry.getKey (), aEntry.getValue (), m_bIsHttpEquiv));
+    return ret;
+  }
+
+  @Override
+  public boolean equals (final Object o)
+  {
+    if (o == this)
+      return true;
+    if (!(o instanceof MetaElement))
+      return false;
+    final MetaElement rhs = (MetaElement) o;
+    return m_sName.equals (rhs.m_sName) &&
+           EqualsUtils.nullSafeEquals (m_sScheme, rhs.m_sScheme) &&
+           m_aContents.equals (rhs.m_aContents) &&
+           m_bIsHttpEquiv == rhs.m_bIsHttpEquiv;
+  }
+
+  @Override
+  public int hashCode ()
+  {
+    return new HashCodeGenerator (this).append (m_sName)
+                                       .append (m_sScheme)
+                                       .append (m_aContents)
+                                       .append (m_bIsHttpEquiv)
+                                       .getHashCode ();
+  }
+
+  @Override
+  public String toString ()
+  {
+    return new ToStringGenerator (this).append ("name", m_sName)
+                                       .appendIfNotNull ("scheme", m_sScheme)
+                                       .append ("contents", m_aContents)
+                                       .append ("isHttpEquiv", m_bIsHttpEquiv)
+                                       .toString ();
+  }
+
+  @Nonnull
+  @Deprecated
+  public static MetaElement createSimpleMetaTag (final String sName, final String sContent, final boolean bIsHttpEquiv)
+  {
+    return new MetaElement (sName, null, bIsHttpEquiv, null, sContent);
+  }
+}
