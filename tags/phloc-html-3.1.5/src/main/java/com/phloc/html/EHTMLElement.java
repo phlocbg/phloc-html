8cb7abb5f9896fbf13496d718024bba0b557c009
ph@phloc.com <ph@phloc.com@bfa6b85e-8048-6ac0-6266-db734edb6865>
2012-05-23 09:44:37 +0000

    [maven-release-plugin]  copy for tag phloc-html-3.1.5

---
+++ tags/phloc-html-3.1.5/src/main/java/com/phloc/html/EHTMLElement.java
@@ -0,0 +1,220 @@
+/**
+ * Copyright (C) 2006-2012 phloc systems
+ * http://www.phloc.com
+ * office[at]phloc[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.phloc.html;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import com.phloc.commons.annotations.Nonempty;
+import com.phloc.commons.string.StringHelper;
+
+/**
+ * Contains a list of all HTML element names.
+ * 
+ * @author philip
+ */
+public enum EHTMLElement
+{
+  A ("a", false),
+  ABBR ("abbr", false),
+  ACRONYM ("acronym", false),
+  ADDRESS ("address", false),
+  APPLET ("applet", false),
+  AREA ("area", true),
+  ARTICLE ("article", false),
+  ASIDE ("aside", false),
+  AUDIO ("audio", false),
+  B ("b", false),
+  BASE ("base", true),
+  BASEFONT ("basefont", true),
+  BDI ("bdi", false),
+  BDO ("bdo", false),
+  BIG ("big", false),
+  BLOCKQUOTE ("blockquote", false),
+  BODY ("body", false),
+  BR ("br", true),
+  BUTTON ("button", false),
+  CANVAS ("canvas", false),
+  CAPTION ("caption", false),
+  CENTER ("center", false),
+  CITE ("cite", false),
+  CODE ("code", false),
+  COL ("col", true),
+  COLGROUP ("colgroup", false),
+  COMMAND ("command", false),
+  DATALIST ("datalist", false),
+  DD ("dd", false),
+  DEL ("del", false),
+  DETAILS ("details", false),
+  DFN ("dfn", false),
+  DIR ("dir", false),
+  DIV ("div", false),
+  DL ("dl", false),
+  DT ("dt", false),
+  EM ("em", false),
+  EMBED ("embed", true),
+  FIELDSET ("fieldset", false),
+  FIGCAPTION ("figcaption", false),
+  FIGURE ("figure", false),
+  FONT ("font", false),
+  FOOTER ("footer", false),
+  FORM ("form", false),
+  FRAME ("frame", true),
+  FRAMESET ("frameset", false),
+  H1 ("h1", false),
+  H2 ("h2", false),
+  H3 ("h3", false),
+  H4 ("h4", false),
+  H5 ("h5", false),
+  H6 ("h6", false),
+  HEAD ("head", false),
+  HEADER ("header", false),
+  HGROUP ("hgroup", false),
+  HR ("hr", true),
+  HTML ("html", false),
+  I ("i", false),
+  IFRAME ("iframe", false),
+  IMG ("img", true),
+  INS ("ins", false),
+  INPUT ("input", true),
+  ISINDEX ("isindex", true),
+  KBD ("kbd", false),
+  KEYGEN ("keygen", true),
+  LABEL ("label", false),
+  LEGEND ("legend", false),
+  LI ("li", false),
+  LINK ("link", false),
+  LISTING ("listing", false),
+  MAP ("map", false),
+  MARK ("mark", false),
+  MENU ("menu", false),
+  META ("meta", true),
+  METER ("meter", false),
+  NAV ("nav", false),
+  NOBR ("nobr", false),
+  NOEMBED ("noembed", false),
+  NOSCRIPT ("noscript", false),
+  OBJECT ("object", false),
+  OL ("ol", false),
+  OPTGROUP ("optgroup", false),
+  OPTION ("option", false),
+  OUTPUT ("output", false),
+  P ("p", false),
+  PARAM ("param", false),
+  PLAINTEXT ("plaintext", false),
+  PRE ("pre", false),
+  PROGRESS ("progress", false),
+  RP ("rp", false),
+  RT ("rt", false),
+  RUBY ("ruby", false),
+  Q ("q", false),
+  S ("s", false),
+  SAMP ("samp", false),
+  SCRIPT ("script", false),
+  SECTION ("section", false),
+  SELECT ("select", false),
+  SMALL ("small", false),
+  SOURCE ("source", true),
+  SPAN ("span", false),
+  STRIKE ("strike", false),
+  STRONG ("strong", false),
+  SUB ("sub", false),
+  SUMMARY ("summary", false),
+  SUP ("sup", false),
+  STYLE ("style", false),
+  TABLE ("table", false),
+  TBODY ("tbody", false),
+  TD ("td", false),
+  TEXTAREA ("textarea", false),
+  TFOOT ("tfoot", false),
+  TH ("th", false),
+  THEAD ("thead", false),
+  TIME ("time", false),
+  TITLE ("title", false),
+  TR ("tr", false),
+  TRACK ("track", true),
+  TT ("tt", false),
+  U ("u", false),
+  UL ("ul", false),
+  VAR ("var", false),
+  VIDEO ("video", false),
+  WBR ("wbr", true),
+  XMP ("xmp", false);
+
+  private static final Set <String> s_aNotSelfClosedElements = new HashSet <String> ();
+
+  private final String m_sElementName;
+  private final boolean m_bMayBeSelfClosed;
+
+  private EHTMLElement (@Nonnull @Nonempty final String sElementName, final boolean bMayBeSelfClosed)
+  {
+    m_sElementName = sElementName;
+    m_bMayBeSelfClosed = bMayBeSelfClosed;
+  }
+
+  @Nonnull
+  @Nonempty
+  public String getElementName ()
+  {
+    return m_sElementName;
+  }
+
+  public boolean mayBeSelfClosed ()
+  {
+    return m_bMayBeSelfClosed;
+  }
+
+  /**
+   * Check if the passed element may not be self closed when creating HTML.
+   * 
+   * @param sElementName
+   *        The name of the tag to validate.
+   * @return <code>true</code> if the tag may not be self closed.
+   */
+  public static boolean isTagThatMayNotBeSelfClosed (@Nullable final String sElementName)
+  {
+    if (s_aNotSelfClosedElements.isEmpty ())
+    {
+      // Lazy init, because it cannot be done in the constructor!
+      for (final EHTMLElement e : values ())
+        if (!e.m_bMayBeSelfClosed)
+          s_aNotSelfClosedElements.add (e.m_sElementName);
+    }
+    return s_aNotSelfClosedElements.contains (sElementName);
+  }
+
+  /**
+   * Check if the passed tag is an HTML tag name.
+   * 
+   * @param sTagName
+   *        The case sensitive tag name to check.
+   * @return <code>true</code> if it is a known HTML tag, <code>false</code>
+   *         otherwise.
+   */
+  public static boolean isHTMLTagName (@Nullable final String sTagName)
+  {
+    if (StringHelper.hasText (sTagName))
+      for (final EHTMLElement e : values ())
+        if (e.getElementName ().equals (sTagName))
+          return true;
+    return false;
+  }
+}
---
+++ tags/phloc-html-3.1.5/src/main/java/com/phloc/html/hc/html/HCVar.java
@@ -0,0 +1,69 @@
+/**
+ * Copyright (C) 2006-2012 phloc systems
+ * http://www.phloc.com
+ * office[at]phloc[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.phloc.html.hc.html;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import com.phloc.commons.text.IPredefinedLocaleTextProvider;
+import com.phloc.html.EHTMLElement;
+import com.phloc.html.hc.IHCNode;
+import com.phloc.html.hc.impl.AbstractHCElementWithChildren;
+
+/**
+ * Represents an HTML &lt;var&gt; element
+ * 
+ * @author philip
+ */
+public final class HCVar extends AbstractHCElementWithChildren <HCVar>
+{
+  public HCVar ()
+  {
+    super (EHTMLElement.VAR);
+  }
+
+  public HCVar (@Nonnull final IPredefinedLocaleTextProvider aChild)
+  {
+    this (aChild.getText ());
+  }
+
+  public HCVar (@Nullable final String sChild)
+  {
+    super (EHTMLElement.VAR, sChild);
+  }
+
+  public HCVar (@Nullable final String... aChildren)
+  {
+    super (EHTMLElement.VAR, aChildren);
+  }
+
+  public HCVar (@Nullable final IHCNode aChild)
+  {
+    super (EHTMLElement.VAR, aChild);
+  }
+
+  public HCVar (@Nullable final IHCNode... aChildren)
+  {
+    super (EHTMLElement.VAR, aChildren);
+  }
+
+  public HCVar (@Nullable final Iterable <? extends IHCNode> aChildren)
+  {
+    super (EHTMLElement.VAR, aChildren);
+  }
+}
