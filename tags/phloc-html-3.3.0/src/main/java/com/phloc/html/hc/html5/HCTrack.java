6a928a8d8c7b31c3893d80fee8f83f11584a33e4
ph@phloc.com <ph@phloc.com@bfa6b85e-8048-6ac0-6266-db734edb6865>
2012-06-18 19:01:29 +0000

    [maven-release-plugin]  copy for tag phloc-html-3.3.0

---
+++ tags/phloc-html-3.3.0/src/main/java/com/phloc/html/hc/html5/HCTrack.java
@@ -0,0 +1,138 @@
+/**
+ * Copyright (C) 2006-2012 phloc systems
+ * http://www.phloc.com
+ * office[at]phloc[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.phloc.html.hc.html5;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import com.phloc.commons.microdom.IMicroElement;
+import com.phloc.commons.string.StringHelper;
+import com.phloc.commons.url.ISimpleURL;
+import com.phloc.html.CHTMLAttributeValues;
+import com.phloc.html.CHTMLAttributes;
+import com.phloc.html.EHTMLElement;
+import com.phloc.html.annotations.SinceHTML5;
+import com.phloc.html.hc.conversion.IHCConversionSettings;
+
+@SinceHTML5
+public final class HCTrack extends AbstractHCMediaElementChild <HCTrack>
+{
+  public static final boolean DEFAULT_DEFAULT = false;
+
+  private String m_sKind;
+  private ISimpleURL m_aSrc;
+  private String m_sSrcLang;
+  private String m_sLabel;
+  private boolean m_bDefault = DEFAULT_DEFAULT;
+
+  public HCTrack ()
+  {
+    super (EHTMLElement.TRACK);
+  }
+
+  public HCTrack (@Nullable final ISimpleURL aSrc)
+  {
+    this ();
+    setSrc (aSrc);
+  }
+
+  @Nullable
+  public String getKind ()
+  {
+    return m_sKind;
+  }
+
+  @Nonnull
+  public HCTrack setKind (@Nullable final String sKind)
+  {
+    m_sKind = sKind;
+    return this;
+  }
+
+  @Nullable
+  public ISimpleURL getSrc ()
+  {
+    return m_aSrc;
+  }
+
+  @Nonnull
+  public HCTrack setSrc (@Nullable final ISimpleURL aSrc)
+  {
+    m_aSrc = aSrc;
+    return this;
+  }
+
+  @Nullable
+  public String getSrcLang ()
+  {
+    return m_sSrcLang;
+  }
+
+  @Nonnull
+  public HCTrack setSrcLang (@Nullable final String sSrcLang)
+  {
+    m_sSrcLang = sSrcLang;
+    return this;
+  }
+
+  @Nullable
+  public String getLabel ()
+  {
+    return m_sLabel;
+  }
+
+  @Nonnull
+  public HCTrack setLabel (@Nullable final String sLabel)
+  {
+    m_sLabel = sLabel;
+    return this;
+  }
+
+  public boolean isDefault ()
+  {
+    return m_bDefault;
+  }
+
+  @Nonnull
+  public HCTrack setDefault (final boolean bDefault)
+  {
+    m_bDefault = bDefault;
+    return this;
+  }
+
+  @Override
+  protected void applyProperties (final IMicroElement aElement, final IHCConversionSettings aConversionSettings)
+  {
+    super.applyProperties (aElement, aConversionSettings);
+    if (StringHelper.hasText (m_sKind))
+      aElement.setAttribute (CHTMLAttributes.KIND, m_sKind);
+    if (m_aSrc != null)
+      aElement.setAttribute (CHTMLAttributes.SRC, m_aSrc.getAsString ());
+    if (StringHelper.hasText (m_sSrcLang))
+      aElement.setAttribute (CHTMLAttributes.SRCLANG, m_sSrcLang);
+    if (StringHelper.hasText (m_sLabel))
+      aElement.setAttribute (CHTMLAttributes.LABEL, m_sLabel);
+    if (m_bDefault)
+      aElement.setAttribute (CHTMLAttributes.DEFAULT, CHTMLAttributeValues.DEFAULT);
+  }
+
+  public String getPlainText ()
+  {
+    return "";
+  }
+}
