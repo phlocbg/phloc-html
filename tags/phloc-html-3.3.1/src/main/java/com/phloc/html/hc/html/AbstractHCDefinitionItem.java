ad9e5ee7ed71aa6e3eed4f2c5086f22469dadcdb
ph@phloc.com <ph@phloc.com@bfa6b85e-8048-6ac0-6266-db734edb6865>
2012-07-13 07:23:31 +0000

    [maven-release-plugin]  copy for tag phloc-html-3.3.1

---
+++ tags/phloc-html-3.3.1/src/main/java/com/phloc/html/hc/html/AbstractHCDefinitionItem.java
@@ -0,0 +1,67 @@
+/**
+ * Copyright (C) 2006-2012 phloc systems
+ * http://www.phloc.com
+ * office[at]phloc[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.phloc.html.hc.html;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import com.phloc.html.EHTMLElement;
+import com.phloc.html.hc.IHCNode;
+import com.phloc.html.hc.impl.AbstractHCElementWithChildren;
+
+/**
+ * Abstract base class for DD and DT tags ({@link HCDD} and {@link HCDT})
+ * 
+ * @author philip
+ * @param <THISTYPE>
+ *        The implementing type
+ */
+public abstract class AbstractHCDefinitionItem <THISTYPE extends AbstractHCDefinitionItem <THISTYPE>> extends
+                                                                                                      AbstractHCElementWithChildren <THISTYPE>
+{
+  public AbstractHCDefinitionItem (@Nonnull final EHTMLElement aElement)
+  {
+    super (aElement);
+  }
+
+  public AbstractHCDefinitionItem (@Nonnull final EHTMLElement aElement, @Nullable final String sChild)
+  {
+    super (aElement, sChild);
+  }
+
+  public AbstractHCDefinitionItem (@Nonnull final EHTMLElement aElement, @Nullable final String... aChildren)
+  {
+    super (aElement, aChildren);
+  }
+
+  public AbstractHCDefinitionItem (@Nonnull final EHTMLElement aElement, @Nullable final IHCNode aChild)
+  {
+    super (aElement, aChild);
+  }
+
+  public AbstractHCDefinitionItem (@Nonnull final EHTMLElement aElement, @Nullable final IHCNode... aChildren)
+  {
+    super (aElement, aChildren);
+  }
+
+  public AbstractHCDefinitionItem (@Nonnull final EHTMLElement aElement,
+                                   @Nullable final Iterable <? extends IHCNode> aChildren)
+  {
+    super (aElement, aChildren);
+  }
+}
