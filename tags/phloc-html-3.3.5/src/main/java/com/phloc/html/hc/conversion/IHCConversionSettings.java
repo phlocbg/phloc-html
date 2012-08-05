3b5ea6c42a778fa5f3a5fe6e6b2714adb6e7b341
ph@phloc.com <ph@phloc.com@bfa6b85e-8048-6ac0-6266-db734edb6865>
2012-08-05 13:23:00 +0000

    [maven-release-plugin]  copy for tag phloc-html-3.3.5

---
+++ tags/phloc-html-3.3.5/src/main/java/com/phloc/html/hc/conversion/IHCConversionSettings.java
@@ -0,0 +1,64 @@
+/**
+ * Copyright (C) 2006-2012 phloc systems
+ * http://www.phloc.com
+ * office[at]phloc[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.phloc.html.hc.conversion;
+
+import javax.annotation.Nonnull;
+
+import com.phloc.css.ECSSVersion;
+import com.phloc.html.EHTMLVersion;
+import com.phloc.html.hc.customize.HCDefaultCustomizer;
+import com.phloc.html.hc.customize.IHCCustomizer;
+
+public interface IHCConversionSettings
+{
+  /**
+   * @return The HTML version to be used to transform HC nodes into XML nodes.
+   */
+  @Nonnull
+  EHTMLVersion getHTMLVersion ();
+
+  /**
+   * @return <code>true</code> if the HTML output should be indented and
+   *         aligned.
+   */
+  boolean isIdentAndAlignHTML ();
+
+  /**
+   * @return The CSS version to be used
+   */
+  @Nonnull
+  ECSSVersion getCSSVersion ();
+
+  /**
+   * @return <code>true</code> if the CSS output should be indented and aligned.
+   */
+  boolean isIdentAndAlignCSS ();
+
+  /**
+   * @return <code>true</code> if the consistency checks are enabled,
+   *         <code>false</code> otherwise.
+   */
+  boolean areConsistencyChecksEnabled ();
+
+  /**
+   * @return The current customizer to be used. Never <code>null</code>. By
+   *         default a {@link HCDefaultCustomizer} object is returned.
+   */
+  @Nonnull
+  IHCCustomizer getCustomizer ();
+}
