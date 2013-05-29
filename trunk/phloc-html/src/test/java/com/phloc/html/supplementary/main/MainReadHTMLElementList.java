5fb950db0355a75ec0271acad4108e3376e45416
ph@phloc.com <ph@phloc.com@bfa6b85e-8048-6ac0-6266-db734edb6865>
2013-05-29 18:51:23 +0000

    Renamed test package

--- trunk/phloc-html/src/test/java/com/phloc/html/supplementary/main/MainReadHTMLElementList.java
+++ trunk/phloc-html/src/test/java/com/phloc/html/supplementary/main/MainReadHTMLElementList.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.phloc.html.tools;
+package com.phloc.html.supplementary.main;
 
 import java.io.File;
 import java.io.InputStream;
