277314af168a67ee9931fc5b5564197fc2a423fb
ph@phloc.com <ph@phloc.com@bfa6b85e-8048-6ac0-6266-db734edb6865>
2013-09-23 21:21:19 +0000

    [maven-release-plugin]  copy for tag phloc-html-3.10.1

---
+++ tags/phloc-html-3.10.1/src/main/java/com/phloc/html/hc/customize/HCEmptyCustomizer.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright (C) 2006-2013 phloc systems
+ * http://www.phloc.com
+ * office[at]phloc[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.phloc.html.hc.customize;
+
+import java.util.List;
+
+import javax.annotation.Nonnull;
+import javax.annotation.concurrent.Immutable;
+
+import com.phloc.commons.string.ToStringGenerator;
+import com.phloc.html.EHTMLVersion;
+import com.phloc.html.hc.IHCNode;
+import com.phloc.html.hc.IHCNodeWithChildren;
+import com.phloc.html.hc.html.HCBody;
+import com.phloc.html.hc.html.HCHead;
+
+/**
+ * An implementation of {@link IHCCustomizer} that does nothing.
+ * 
+ * @author Philip Helger
+ */
+@Immutable
+public class HCEmptyCustomizer implements IHCCustomizer
+{
+  public HCEmptyCustomizer ()
+  {}
+
+  public void customizeNode (@Nonnull final IHCNodeWithChildren <?> aParentElement,
+                             @Nonnull final IHCNode aNode,
+                             @Nonnull final EHTMLVersion eHTMLVersion)
+  {
+    // Nada
+  }
+
+  public void handleOutOfBandNodes (@Nonnull final List <IHCNode> aOutOfBandNodes,
+                                    @Nonnull final HCHead aHead,
+                                    @Nonnull final HCBody aBody)
+  {
+    // Nada
+  }
+
+  @Override
+  public String toString ()
+  {
+    return new ToStringGenerator (this).toString ();
+  }
+}
---
+++ tags/phloc-html-3.10.1/src/main/java/com/phloc/html/js/builder/JSAssocArray.java
@@ -0,0 +1,205 @@
+/**
+ * Copyright (C) 2006-2013 phloc systems
+ * http://www.phloc.com
+ * office[at]phloc[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.phloc.html.js.builder;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import javax.annotation.Nonnegative;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import com.phloc.commons.equals.EqualsUtils;
+import com.phloc.commons.hash.HashCodeGenerator;
+import com.phloc.commons.string.ToStringGenerator;
+import com.phloc.html.js.marshal.JSMarshaller;
+import com.phloc.json.IJSON;
+
+/**
+ * array creation and initialization.
+ * 
+ * @author Philip Helger
+ */
+public class JSAssocArray extends AbstractJSExpression
+{
+  private Map <IJSExpression, IJSExpression> m_aExprs;
+
+  public JSAssocArray ()
+  {}
+
+  public boolean isEmpty ()
+  {
+    return m_aExprs == null || m_aExprs.isEmpty ();
+  }
+
+  @Nonnegative
+  public int size ()
+  {
+    return m_aExprs == null ? 0 : m_aExprs.size ();
+  }
+
+  @Nonnull
+  public JSAssocArray add (@Nonnull final String sKey, final boolean bValue)
+  {
+    return add (sKey, JSExpr.lit (bValue));
+  }
+
+  @Nonnull
+  public JSAssocArray add (@Nonnull final String sKey, final char cValue)
+  {
+    return add (sKey, JSExpr.lit (cValue));
+  }
+
+  @Nonnull
+  public JSAssocArray add (@Nonnull final String sKey, final double dValue)
+  {
+    return add (sKey, JSExpr.lit (dValue));
+  }
+
+  @Nonnull
+  public JSAssocArray add (@Nonnull final String sKey, final float fValue)
+  {
+    return add (sKey, JSExpr.lit (fValue));
+  }
+
+  @Nonnull
+  public JSAssocArray add (@Nonnull final String sKey, final int nValue)
+  {
+    return add (sKey, JSExpr.lit (nValue));
+  }
+
+  @Nonnull
+  public JSAssocArray add (@Nonnull final String sKey, final long nValue)
+  {
+    return add (sKey, JSExpr.lit (nValue));
+  }
+
+  @Nonnull
+  public JSAssocArray add (@Nonnull final String sKey, @Nullable final String sValue)
+  {
+    return add (sKey, sValue == null ? JSExpr.NULL : JSExpr.lit (sValue));
+  }
+
+  @Nonnull
+  public JSAssocArray add (@Nonnull final String sKey, @Nullable final IJSON aValue)
+  {
+    return add (sKey, aValue == null ? JSExpr.NULL : JSExpr.json (aValue));
+  }
+
+  @Nonnull
+  public JSAssocArray addAllStrings (@Nullable final Map <String, String> aValues)
+  {
+    if (aValues != null)
+      for (final Map.Entry <String, String> aEntry : aValues.entrySet ())
+        add (aEntry.getKey (), JSExpr.lit (aEntry.getValue ()));
+    return this;
+  }
+
+  @Nonnull
+  public JSAssocArray addAll (@Nullable final Map <String, IJSExpression> aValues)
+  {
+    if (aValues != null)
+      for (final Map.Entry <String, IJSExpression> aEntry : aValues.entrySet ())
+        add (aEntry.getKey (), aEntry.getValue ());
+    return this;
+  }
+
+  @Nonnull
+  public JSAssocArray add (@Nonnull final String sKey, @Nonnull final IJSExpression aValue)
+  {
+    // Don't quote value identifiers
+    if (JSMarshaller.isJSIdentifier (sKey))
+      return add (new JSAtom (sKey), aValue);
+
+    return add (JSExpr.lit (sKey), aValue);
+  }
+
+  /**
+   * Add an element to the array initializer
+   */
+  @Nonnull
+  public JSAssocArray add (@Nonnull final IJSExpression aKey, @Nonnull final IJSExpression aValue)
+  {
+    if (aKey == null)
+      throw new NullPointerException ("key");
+    if (aValue == null)
+      throw new NullPointerException ("value");
+
+    if (m_aExprs == null)
+      m_aExprs = new LinkedHashMap <IJSExpression, IJSExpression> ();
+    m_aExprs.put (aKey, aValue);
+    return this;
+  }
+
+  @Nonnull
+  public JSAssocArray remove (@Nonnull final String sKey)
+  {
+    if (m_aExprs != null)
+      remove (JSExpr.lit (sKey));
+    return this;
+  }
+
+  @Nonnull
+  public JSAssocArray remove (@Nullable final IJSExpression aKey)
+  {
+    if (m_aExprs != null)
+      m_aExprs.remove (aKey);
+    return this;
+  }
+
+  public void generate (@Nonnull final JSFormatter aFormatter)
+  {
+    aFormatter.plain ('{').nl ().indent ();
+    if (m_aExprs != null)
+    {
+      boolean bFirst = true;
+      for (final Map.Entry <IJSExpression, IJSExpression> aEntry : m_aExprs.entrySet ())
+      {
+        if (bFirst)
+          bFirst = false;
+        else
+          aFormatter.plain (',').nl ();
+        aFormatter.generatable (aEntry.getKey ()).plain (':').generatable (aEntry.getValue ());
+      }
+    }
+    aFormatter.nl ().outdent ().plain ('}');
+  }
+
+  @Override
+  public boolean equals (final Object o)
+  {
+    if (o == this)
+      return true;
+    if (!super.equals (o))
+      return false;
+    final JSAssocArray rhs = (JSAssocArray) o;
+    return EqualsUtils.equals (m_aExprs, rhs.m_aExprs);
+  }
+
+  @Override
+  public int hashCode ()
+  {
+    return HashCodeGenerator.getDerived (super.hashCode ()).append (m_aExprs).getHashCode ();
+  }
+
+  @Override
+  public String toString ()
+  {
+    return ToStringGenerator.getDerived (super.toString ()).append ("exprs", m_aExprs).toString ();
+  }
+}
