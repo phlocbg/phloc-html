af8a7471b1a8dea668adf0d548ea93ce0d28fe30
ph@phloc.com <ph@phloc.com@bfa6b85e-8048-6ac0-6266-db734edb6865>
2014-02-28 15:46:49 +0000

    Changed package names

---
+++ trunk/phloc-html/src/main/java/com/phloc/html/markdown/ext/CodeBlockEmitter.java
@@ -0,0 +1,43 @@
+package com.phloc.html.markdown.ext;
+
+import java.util.List;
+
+import com.phloc.html.markdown.IBlockEmitter;
+
+public class CodeBlockEmitter implements IBlockEmitter
+{
+  @Override
+  public void emitBlock (final StringBuilder out, final List <String> lines, final String meta)
+  {
+    out.append ("<pre><code");
+    if (meta.length () > 0)
+    {
+      out.append (" class=\"" + meta + "\"");
+    }
+    out.append (">");
+    for (final String s : lines)
+    {
+      for (int i = 0; i < s.length (); i++)
+      {
+        final char c = s.charAt (i);
+        switch (c)
+        {
+          case '&':
+            out.append ("&amp;");
+            break;
+          case '<':
+            out.append ("&lt;");
+            break;
+          case '>':
+            out.append ("&gt;");
+            break;
+          default:
+            out.append (c);
+            break;
+        }
+      }
+      out.append ('\n');
+    }
+    out.append ("</code></pre>\n");
+  }
+}
