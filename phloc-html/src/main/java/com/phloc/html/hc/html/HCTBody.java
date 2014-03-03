a5b0b93a373ca711b40bbcc62b6bcc9494068880
ph <ph@phloc.com>
2014-03-03 11:15:10 +0000

    Started making explicit elements for thead, tbody and tfoot

---
+++ phloc-html/src/main/java/com/phloc/html/hc/html/HCTBody.java
@@ -0,0 +1,16 @@
+package com.phloc.html.hc.html;
+
+import com.phloc.html.EHTMLElement;
+
+/**
+ * HTML &lt;tbody&gt; element.
+ * 
+ * @author Philip Helger
+ */
+public class HCTBody extends AbstractHCTablePart <HCTBody>
+{
+  public HCTBody ()
+  {
+    super (EHTMLElement.TBODY, false);
+  }
+}
---
+++ phloc-html/src/main/java/com/phloc/html/hc/html/HCTHead.java
@@ -0,0 +1,16 @@
+package com.phloc.html.hc.html;
+
+import com.phloc.html.EHTMLElement;
+
+/**
+ * HTML &lt;thead&gt; element.
+ * 
+ * @author Philip Helger
+ */
+public class HCTHead extends AbstractHCTablePart <HCTHead>
+{
+  public HCTHead ()
+  {
+    super (EHTMLElement.THEAD, true);
+  }
+}
