44535ebb3e645becbc7b883c07703c08b417c6a3
ph <ph@phloc.com>
2014-03-21 15:22:29 +0000

    Minor improvements

--- phloc-html/src/test/java/com/phloc/html/hc/impl/HCConditionalCommentNodeTest.java
+++ phloc-html/src/test/java/com/phloc/html/hc/impl/HCConditionalCommentNodeTest.java
@@ -18,15 +18,20 @@
 package com.phloc.html.hc.impl;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 import org.junit.Test;
 
+import com.phloc.commons.GlobalDebug;
+import com.phloc.commons.xml.serialize.EXMLSerializeIndent;
+import com.phloc.commons.xml.serialize.IXMLWriterSettings;
+import com.phloc.html.hc.conversion.HCConversionSettings;
 import com.phloc.html.hc.conversion.HCSettings;
 import com.phloc.html.hc.html.HCB;
 
 /**
  * Test class for class {@link HCConditionalCommentNode}
- * 
+ *
  * @author Philip Helger
  */
 public final class HCConditionalCommentNodeTest
@@ -35,13 +40,40 @@ public final class HCConditionalCommentNodeTest
   public void testAll ()
   {
     HCConditionalCommentNode.setDefaultLineSeparator ("\n");
-    assertEquals ("<!--[if IE]>\nabc<![endif]-->",
-                  HCSettings.getAsHTMLString (HCConditionalCommentNode.createForIE (new HCTextNode ("abc")), false));
-    assertEquals ("<!--[if IE]>\n" + "<b xmlns=\"http://www.w3.org/1999/xhtml\">bold</b>" + "<![endif]-->",
-                  HCSettings.getAsHTMLString (HCConditionalCommentNode.createForIE (new HCB ().addChild ("bold")),
-                                              false));
-    assertEquals ("<!--[if IE]>\n" + "<b>bold</b>" + "<![endif]-->",
+    HCSettings.getConversionSettingsProvider ()
+              .setXMLWriterSettings (HCConversionSettings.createDefaultXMLWriterSettings ()
+                                                         .setIndent (EXMLSerializeIndent.NONE));
+
+    // By default, in non-debug mode, no newline is emitted after comment start
+    assertFalse (GlobalDebug.isDebugMode ());
+    assertEquals ("<!--[if IE]>" + "abc" + "<![endif]-->",
+                  HCSettings.getAsHTMLString (HCConditionalCommentNode.createForIE (new HCTextNode ("abc")), true));
+    assertEquals ("<!--[if IE]>" + "<b xmlns=\"http://www.w3.org/1999/xhtml\">bold</b>" + "<![endif]-->",
+                  HCSettings.getAsHTMLString (HCConditionalCommentNode.createForIE (new HCB ().addChild ("bold")), true));
+    assertEquals ("<!--[if IE 7]>" + "<b>bold</b>" + "<![endif]-->",
+                  HCSettings.getAsHTMLStringWithoutNamespaces (HCConditionalCommentNode.createForIEExactVersion7 (new HCB ().addChild ("bold")),
+                                                               true));
+
+    // But if enabling alignment mode, the newline is printer!
+    final IXMLWriterSettings aXWS = HCConversionSettings.createDefaultXMLWriterSettings ()
+                                                        .setIndent (EXMLSerializeIndent.ALIGN_ONLY);
+    final String sCRLF = aXWS.getNewlineString ();
+    HCSettings.getConversionSettingsProvider ().setXMLWriterSettings (aXWS);
+
+    assertEquals ("<!--[if IE]>\n" + "abc" + "<![endif]-->" + sCRLF,
+                  HCSettings.getAsHTMLString (HCConditionalCommentNode.createForIE (new HCTextNode ("abc")), true));
+    assertEquals ("<!--[if IE]>\n" +
+                      "<b xmlns=\"http://www.w3.org/1999/xhtml\">bold</b>" +
+                      sCRLF +
+                      "<![endif]-->" +
+                      sCRLF,
+                  HCSettings.getAsHTMLString (HCConditionalCommentNode.createForIE (new HCB ().addChild ("bold")), true));
+    assertEquals ("<!--[if IE]>\n" + "<b>bold</b>" + sCRLF + "<![endif]-->" + sCRLF,
                   HCSettings.getAsHTMLStringWithoutNamespaces (HCConditionalCommentNode.createForIE (new HCB ().addChild ("bold")),
-                                                               false));
+                                                               true));
+
+    // Restore default settings
+    HCSettings.getConversionSettingsProvider ()
+              .setXMLWriterSettings (HCConversionSettings.createDefaultXMLWriterSettings ());
   }
 }
