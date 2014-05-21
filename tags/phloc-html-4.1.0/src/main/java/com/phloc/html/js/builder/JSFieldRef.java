62bf7c805037297803c64328c9c708f04a78d7aa
ph@phloc.com <ph@phloc.com@bfa6b85e-8048-6ac0-6266-db734edb6865>
2014-05-21 21:51:48 +0000

    [maven-release-plugin]  copy for tag phloc-html-4.1.0

---
+++ tags/phloc-html-4.1.0/src/main/java/com/phloc/html/js/builder/JSFieldRef.java
@@ -0,0 +1,86 @@
+/**
+ * Copyright (C) 2006-2014 phloc systems
+ * http://www.phloc.com
+ * office[at]phloc[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.phloc.html.js.builder;
+
+import javax.annotation.Nonnull;
+
+import com.phloc.commons.ValueEnforcer;
+import com.phloc.commons.annotations.Nonempty;
+import com.phloc.commons.hash.HashCodeGenerator;
+import com.phloc.commons.string.ToStringGenerator;
+
+/**
+ * Field Reference
+ *
+ * @author Philip Helger
+ */
+public class JSFieldRef extends JSRef
+{
+  /**
+   * Object expression upon which this field will be accessed, or null for the
+   * implicit 'this'.
+   */
+  private final IJSGeneratable m_aObject;
+
+  public JSFieldRef (@Nonnull final IJSGeneratable aObject, @Nonnull @Nonempty final String sName)
+  {
+    super (sName);
+    m_aObject = ValueEnforcer.notNull (aObject, "Object");
+  }
+
+  public JSFieldRef (@Nonnull final IJSGeneratable aObject, @Nonnull final JSVar aVar)
+  {
+    super (aVar);
+    m_aObject = ValueEnforcer.notNull (aObject, "Object");
+  }
+
+  @Nonnull
+  public IJSGeneratable object ()
+  {
+    return m_aObject;
+  }
+
+  @Override
+  public void generate (@Nonnull final JSFormatter aFormatter)
+  {
+    aFormatter.generatable (m_aObject).plain ('.').plain (name ());
+  }
+
+  @Override
+  public boolean equals (final Object o)
+  {
+    if (o == this)
+      return true;
+    if (!super.equals (o))
+      return false;
+    final JSFieldRef rhs = (JSFieldRef) o;
+    return m_aObject.equals (rhs.m_aObject);
+  }
+
+  @Override
+  public int hashCode ()
+  {
+    return HashCodeGenerator.getDerived (super.hashCode ()).append (m_aObject).getHashCode ();
+  }
+
+  @Override
+  public String toString ()
+  {
+    return ToStringGenerator.getDerived (super.toString ()).append ("object", m_aObject).toString ();
+  }
+}
---
+++ tags/phloc-html-4.1.0/src/main/java/com/phloc/html/markdown/MarkdownProcessingResult.java
@@ -0,0 +1,65 @@
+/**
+ * Copyright (C) 2006-2014 phloc systems
+ * http://www.phloc.com
+ * office[at]phloc[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.phloc.html.markdown;
+
+import javax.annotation.Nonnull;
+
+import com.phloc.html.hc.conversion.HCSettings;
+import com.phloc.html.hc.conversion.IHCConversionSettings;
+import com.phloc.html.hc.impl.HCNodeList;
+
+/**
+ * The result of a Markdown processing
+ *
+ * @author Philip Helger
+ */
+public class MarkdownProcessingResult
+{
+  private final HCNodeList m_aNodeList;
+
+  public MarkdownProcessingResult (@Nonnull final HCStack aResult)
+  {
+    if (aResult == null)
+      throw new NullPointerException ("Result");
+    m_aNodeList = aResult.getRoot ();
+  }
+
+  @Nonnull
+  public HCNodeList getNodeList ()
+  {
+    return m_aNodeList;
+  }
+
+  @Nonnull
+  public String getAsHTMLString ()
+  {
+    return getAsHTMLString (HCSettings.isDefaultPrettyPrint ());
+  }
+
+  @Nonnull
+  public String getAsHTMLString (final boolean bPrettyPrint)
+  {
+    return HCSettings.getAsHTMLStringWithoutNamespaces (m_aNodeList, bPrettyPrint).trim ();
+  }
+
+  @Nonnull
+  public String getAsHTMLString (@Nonnull final IHCConversionSettings aConversionSettings)
+  {
+    return HCSettings.getAsHTMLString (m_aNodeList, aConversionSettings).trim ();
+  }
+}
