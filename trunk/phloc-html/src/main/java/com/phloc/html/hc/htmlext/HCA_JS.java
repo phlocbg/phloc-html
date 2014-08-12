a4a2990cdf3054931dd405e8236258db65d9757b
ph@phloc.com <ph@phloc.com@bfa6b85e-8048-6ac0-6266-db734edb6865>
2014-08-12 12:38:13 +0000

    Extracted AbstractHCA

--- trunk/phloc-html/src/main/java/com/phloc/html/hc/htmlext/HCA_JS.java
+++ trunk/phloc-html/src/main/java/com/phloc/html/hc/htmlext/HCA_JS.java
@@ -22,29 +22,55 @@ import javax.annotation.Nullable;
 
 import com.phloc.commons.url.ISimpleURL;
 import com.phloc.commons.url.ReadonlySimpleURL;
-import com.phloc.html.hc.html.HCA;
+import com.phloc.html.hc.html.AbstractHCA;
 import com.phloc.html.js.CJS;
 import com.phloc.html.js.IJSCodeProvider;
 import com.phloc.html.js.builder.JSExpr;
 import com.phloc.html.js.builder.JSReturn;
 import com.phloc.html.js.provider.CollectingJSCodeProvider;
 
-public class HCA_JS extends HCA
+public class HCA_JS extends AbstractHCA <HCA_JS>
 {
   public static final ISimpleURL JS_URL = new ReadonlySimpleURL (CJS.JS_VOID);
 
-  public HCA_JS (@Nullable final IJSCodeProvider aJSOnClick)
+  public HCA_JS ()
   {
     /**
      * JS links must always set a JS void in the href. This ensures that if the
      * href gets called (due to double click etc.) it has no effect.
      **/
     super (JS_URL);
-    setOnClickReturnFalse (this, aJSOnClick);
   }
 
-  public static void setOnClickReturnFalse (@Nonnull final HCA aLink, @Nullable final IJSCodeProvider aJSOnClick)
+  public HCA_JS (@Nullable final IJSCodeProvider aJSOnClick)
+  {
+    this ();
+    setOnClickReturnFalse (aJSOnClick);
+  }
+
+  @Nonnull
+  public final HCA_JS addOnClickReturnFalse (@Nullable final IJSCodeProvider aJSOnClick)
+  {
+    addOnClickReturnFalse (this, aJSOnClick);
+    return this;
+  }
+
+  @Nonnull
+  public final HCA_JS setOnClickReturnFalse (@Nullable final IJSCodeProvider aJSOnClick)
+  {
+    setOnClickReturnFalse (this, aJSOnClick);
+    return this;
+  }
+
+  public static void addOnClickReturnFalse (@Nonnull final AbstractHCA <?> aLink,
+                                            @Nullable final IJSCodeProvider aJSOnClick)
   {
     aLink.addOnClick (new CollectingJSCodeProvider (aJSOnClick, new JSReturn (JSExpr.FALSE)));
+  }
+
+  public static void setOnClickReturnFalse (@Nonnull final AbstractHCA <?> aLink,
+                                            @Nullable final IJSCodeProvider aJSOnClick)
+  {
+    aLink.setOnClick (new CollectingJSCodeProvider (aJSOnClick, new JSReturn (JSExpr.FALSE)));
   }
 }
