bc1299ed6e5995ff2cbcfbd75822722ad3d5f85b
Boris Gregorcic <bg@phloc.com>
2018-07-31 10:35:33 +0200

    Removed warning on more than 31 included CSS files (concerns IE 8 and lower).

--- phloc-html/src/main/java/com/phloc/html/hc/html/HCHead.java
+++ phloc-html/src/main/java/com/phloc/html/hc/html/HCHead.java
@@ -26,10 +26,6 @@ import javax.annotation.Nonnegative;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import com.phloc.commons.GlobalDebug;
 import com.phloc.commons.ValueEnforcer;
 import com.phloc.commons.annotations.OverrideOnDemand;
 import com.phloc.commons.annotations.ReturnsMutableCopy;
@@ -42,7 +38,6 @@ import com.phloc.commons.string.ToStringGenerator;
 import com.phloc.commons.url.ISimpleURL;
 import com.phloc.html.CHTMLAttributes;
 import com.phloc.html.EHTMLElement;
-import com.phloc.html.hc.IHCCSSNode;
 import com.phloc.html.hc.IHCNode;
 import com.phloc.html.hc.api.EHCLinkType;
 import com.phloc.html.hc.api.IHCLinkType;
@@ -54,79 +49,75 @@ import com.phloc.html.meta.IMetaElement;
 /**
  * Represents an HTML &lt;head&gt; element
  * 
+ * @author Boris Gregorcic
  * @author Philip Helger
  */
 public class HCHead extends AbstractHCElement <HCHead>
 {
-  private static final int MAX_CSS_IE = 31;
-  private static final Logger s_aLogger = LoggerFactory.getLogger (HCHead.class);
+  private static final long serialVersionUID = 664545572494794975L;
 
   private String m_sProfile;
   private final HCTitle m_aPageTitle = new HCTitle ();
   private final HCBase m_aBase = new HCBase ();
-  private final Map <String, IMetaElement> m_aMetaElements = new LinkedHashMap <String, IMetaElement> ();
-  private final List <HCLink> m_aLinks = new ArrayList <HCLink> ();
-  private final List <IHCNode> m_aCSS = new ArrayList <IHCNode> ();
-  private final List <IHCNode> m_aJS = new ArrayList <IHCNode> ();
+  private final Map <String, IMetaElement> m_aMetaElements = new LinkedHashMap <> ();
+  private final List <HCLink> m_aLinks = new ArrayList <> ();
+  private final List <IHCNode> m_aCSS = new ArrayList <> ();
+  private final List <IHCNode> m_aJS = new ArrayList <> ();
 
   public HCHead ()
   {
     super (EHTMLElement.HEAD);
   }
 
-  //
-  // Head fields/attributes
-  //
-
   @Nullable
   public String getProfile ()
   {
-    return m_sProfile;
+    return this.m_sProfile;
   }
 
   @Nonnull
   public HCHead setProfile (@Nullable final String sProfile)
   {
-    m_sProfile = sProfile;
+    this.m_sProfile = sProfile;
     return this;
   }
 
   @Nullable
   public String getPageTitle ()
   {
-    return m_aPageTitle.getContent ();
+    return this.m_aPageTitle.getContent ();
   }
 
   @Nonnull
   public HCHead setPageTitle (@Nullable final String sPageTitle)
   {
-    m_aPageTitle.setContent (sPageTitle);
+    this.m_aPageTitle.setContent (sPageTitle);
     return this;
   }
 
   @Nullable
   public String getBaseHref ()
   {
-    return m_aBase.getHref ();
+    return this.m_aBase.getHref ();
   }
 
   @Nonnull
   public HCHead setBaseHref (@Nullable final String sBaseHref)
   {
-    m_aBase.setHref (sBaseHref);
+    this.m_aBase.setHref (sBaseHref);
     return this;
   }
 
   @Nullable
   public HCA_Target getBaseTarget ()
   {
-    return m_aBase.getTarget ();
+    return this.m_aBase.getTarget ();
   }
 
   @Nonnull
   public HCHead setBaseTarget (@Nullable final HCA_Target aTarget)
   {
-    m_aBase.setTarget (aTarget);
+    this.m_aBase.setTarget (aTarget);
     return this;
   }
 
@@ -137,28 +128,28 @@ public class HCHead extends AbstractHCElement <HCHead>
   @Nonnull
   public HCHead addMetaElement (@Nonnull final IMetaElement aMetaElement)
   {
-    ValueEnforcer.notNull (aMetaElement, "MetaElement");
-    m_aMetaElements.put (aMetaElement.getName (), aMetaElement);
+    ValueEnforcer.notNull (aMetaElement, "MetaElement"); //$NON-NLS-1$
+    this.m_aMetaElements.put (aMetaElement.getName (), aMetaElement);
     return this;
   }
 
   @Nonnull
   public EChange removeMetaElement (@Nullable final String sMetaElementName)
   {
-    return EChange.valueOf (m_aMetaElements.remove (sMetaElementName) != null);
+    return EChange.valueOf (this.m_aMetaElements.remove (sMetaElementName) != null);
   }
 
   @Nonnull
   @ReturnsMutableCopy
   public List <IMetaElement> getAllMetaElements ()
   {
-    return ContainerHelper.newList (m_aMetaElements.values ());
+    return ContainerHelper.newList (this.m_aMetaElements.values ());
   }
 
   @Nonnegative
   public int getMetaElementCount ()
   {
-    return m_aMetaElements.size ();
+    return this.m_aMetaElements.size ();
   }
 
   //
@@ -193,8 +184,8 @@ public class HCHead extends AbstractHCElement <HCHead>
   @Nonnull
   public HCHead addLink (@Nonnull final HCLink aLink)
   {
-    ValueEnforcer.notNull (aLink, "Link");
-    m_aLinks.add (aLink);
+    ValueEnforcer.notNull (aLink, "Link"); //$NON-NLS-1$
+    this.m_aLinks.add (aLink);
     return this;
   }
 
@@ -211,8 +202,8 @@ public class HCHead extends AbstractHCElement <HCHead>
   @Nonnull
   public HCHead addLink (@Nonnegative final int nIndex, @Nonnull final HCLink aLink)
   {
-    ValueEnforcer.notNull (aLink, "Link");
-    m_aLinks.add (nIndex, aLink);
+    ValueEnforcer.notNull (aLink, "Link"); //$NON-NLS-1$
+    this.m_aLinks.add (nIndex, aLink);
     return this;
   }
 
@@ -220,11 +211,11 @@ public class HCHead extends AbstractHCElement <HCHead>
   public EChange removeLinkOfRel (@Nonnull final IHCLinkType aLinkType)
   {
     int i = 0;
-    for (final HCLink aLink : m_aLinks)
+    for (final HCLink aLink : this.m_aLinks)
     {
       if (aLink.getRel ().equals (aLinkType))
       {
-        m_aLinks.remove (i);
+        this.m_aLinks.remove (i);
         return EChange.CHANGED;
       }
       ++i;
@@ -236,71 +227,67 @@ public class HCHead extends AbstractHCElement <HCHead>
   @ReturnsMutableCopy
   public List <HCLink> getAllLinks ()
   {
-    return ContainerHelper.newList (m_aLinks);
+    return ContainerHelper.newList (this.m_aLinks);
   }
 
   @Nonnegative
   public int getLinkCount ()
   {
-    return m_aLinks.size ();
+    return this.m_aLinks.size ();
   }
-
-  //
-  // CSS handling
-  //
 
   @Nonnull
   public HCHead addCSS (@Nonnull final IHCNode aCSS)
   {
-    ValueEnforcer.notNull (aCSS, "CSS");
+    ValueEnforcer.notNull (aCSS, "CSS"); //$NON-NLS-1$
     if (!HCSpecialNodeHandler.isCSSNode (aCSS))
-      throw new IllegalArgumentException (aCSS + " is not a valid CSS node!");
-    m_aCSS.add (aCSS);
+    {
+      throw new IllegalArgumentException (aCSS + " is not a valid CSS node!"); //$NON-NLS-1$
+    }
+    this.m_aCSS.add (aCSS);
     return this;
   }
 
   @Nonnull
   public HCHead addCSS (@Nonnegative final int nIndex, @Nonnull final IHCNode aCSS)
   {
-    ValueEnforcer.notNull (aCSS, "CSS");
+    ValueEnforcer.notNull (aCSS, "CSS"); //$NON-NLS-1$
     if (!HCSpecialNodeHandler.isCSSNode (aCSS))
-      throw new IllegalArgumentException (aCSS + " is not a valid CSS node!");
-    m_aCSS.add (nIndex, aCSS);
+    {
+      throw new IllegalArgumentException (aCSS + " is not a valid CSS node!"); //$NON-NLS-1$
+    }
+    this.m_aCSS.add (nIndex, aCSS);
     return this;
   }
 
   @Nonnegative
   public int getCSSCount ()
   {
-    return m_aCSS.size ();
+    return this.m_aCSS.size ();
   }
 
   @Nonnull
   @ReturnsMutableCopy
   public List <IHCNode> getAllCSSNodes ()
   {
-    return ContainerHelper.newList (m_aCSS);
+    return ContainerHelper.newList (this.m_aCSS);
   }
 
   @Nonnull
   @ReturnsMutableCopy
   public List <IHCNode> getAllAndRemoveAllCSSNodes ()
   {
-    final List <IHCNode> ret = ContainerHelper.newList (m_aCSS);
-    m_aCSS.clear ();
+    final List <IHCNode> ret = ContainerHelper.newList (this.m_aCSS);
+    this.m_aCSS.clear ();
     return ret;
   }
 
   @Nonnull
   public HCHead removeAllCSS ()
   {
-    m_aCSS.clear ();
+    this.m_aCSS.clear ();
     return this;
   }
-
-  //
-  // JS handling
-  //
 
   /**
    * Append some JavaScript code
@@ -312,10 +299,12 @@ public class HCHead extends AbstractHCElement <HCHead>
   @Nonnull
   public HCHead addJS (@Nonnull final IHCNode aJS)
   {
-    ValueEnforcer.notNull (aJS, "JS");
+    ValueEnforcer.notNull (aJS, "JS"); //$NON-NLS-1$
     if (!HCSpecialNodeHandler.isJSNode (aJS))
-      throw new IllegalArgumentException (aJS + " is not a valid JS node!");
-    m_aJS.add (aJS);
+    {
+      throw new IllegalArgumentException (aJS + " is not a valid JS node!"); //$NON-NLS-1$
+    }
+    this.m_aJS.add (aJS);
     return this;
   }
 
@@ -331,10 +320,12 @@ public class HCHead extends AbstractHCElement <HCHead>
   @Nonnull
   public HCHead addJS (@Nonnegative final int nIndex, @Nonnull final IHCNode aJS)
   {
-    ValueEnforcer.notNull (aJS, "JS");
+    ValueEnforcer.notNull (aJS, "JS"); //$NON-NLS-1$
     if (!HCSpecialNodeHandler.isJSNode (aJS))
-      throw new IllegalArgumentException (aJS + " is not a valid JS node!");
-    m_aJS.add (nIndex, aJS);
+    {
+      throw new IllegalArgumentException (aJS + " is not a valid JS node!"); //$NON-NLS-1$
+    }
+    this.m_aJS.add (nIndex, aJS);
     return this;
   }
 
@@ -344,71 +335,60 @@ public class HCHead extends AbstractHCElement <HCHead>
   @Nonnegative
   public int getJSCount ()
   {
-    return m_aJS.size ();
+    return this.m_aJS.size ();
   }
 
   @Nonnull
   @ReturnsMutableCopy
   public List <IHCNode> getAllJSNodes ()
   {
-    return ContainerHelper.newList (m_aJS);
+    return ContainerHelper.newList (this.m_aJS);
   }
 
   @Nonnull
   @ReturnsMutableCopy
   public List <IHCNode> getAllAndRemoveAllJSNodes ()
   {
-    final List <IHCNode> ret = ContainerHelper.newList (m_aJS);
-    m_aJS.clear ();
+    final List <IHCNode> ret = ContainerHelper.newList (this.m_aJS);
+    this.m_aJS.clear ();
     return ret;
   }
 
   @Nonnull
   public HCHead removeAllJS ()
   {
-    m_aJS.clear ();
+    this.m_aJS.clear ();
     return this;
   }
-
-  //
-  // Code generation
-  //
 
   @OverrideOnDemand
   protected void emitLinks (@Nonnull final IMicroElement eHead,
                             @Nonnull final IHCConversionSettingsToNode aConversionSettings)
   {
-    for (final HCLink aLink : m_aLinks)
+    for (final HCLink aLink : this.m_aLinks)
+    {
       eHead.appendChild (aLink.convertToNode (aConversionSettings));
+    }
   }
 
   @OverrideOnDemand
   protected void emitCSS (@Nonnull final IMicroElement eHead,
                           @Nonnull final IHCConversionSettingsToNode aConversionSettings)
   {
-    int nCSSExternals = 0;
-    for (final IHCNode aCSS : m_aCSS)
+    for (final IHCNode aCSS : this.m_aCSS)
     {
-      if (aCSS instanceof IHCCSSNode && !((IHCCSSNode) aCSS).isInlineCSS ())
-        ++nCSSExternals;
       eHead.appendChild (aCSS.convertToNode (aConversionSettings));
     }
-
-    // Sources:
-    // http://acidmartin.wordpress.com/2008/11/25/the-32-external-css-files-limitation-of-internet-explorer-and-more/
-    // http://social.msdn.microsoft.com/Forums/en-US/iewebdevelopment/thread/ad1b6e88-bbfa-4cc4-9e95-3889b82a7c1d
-    if (nCSSExternals > MAX_CSS_IE && !GlobalDebug.isDebugMode ())
-      s_aLogger.warn ("You are including more than 31 CSS files (" +
-                      nCSSExternals +
-                      ") in your request, which will be ignored by Internet Explorer (at least up to version 8)!");
   }
 
   @OverrideOnDemand
   protected void emitJS (@Nonnull final IMicroElement eHead,
                          @Nonnull final IHCConversionSettingsToNode aConversionSettings)
   {
-    for (final IHCNode aJS : m_aJS)
+    for (final IHCNode aJS : this.m_aJS)
+    {
       eHead.appendChild (aJS.convertToNode (aConversionSettings));
+    }
   }
 
   @Override
@@ -417,18 +397,22 @@ public class HCHead extends AbstractHCElement <HCHead>
   {
     super.applyProperties (eHead, aConversionSettings);
 
-    if (StringHelper.hasText (m_sProfile))
-      eHead.setAttribute (CHTMLAttributes.PROFILE, m_sProfile);
+    if (StringHelper.hasText (this.m_sProfile))
+    {
+      eHead.setAttribute (CHTMLAttributes.PROFILE, this.m_sProfile);
+    }
 
     // Append meta element first for charset encoding!
-    for (final IMetaElement aMetaElement : m_aMetaElements.values ())
+    for (final IMetaElement aMetaElement : this.m_aMetaElements.values ())
+    {
       eHead.appendChild (aMetaElement.convertToNode (aConversionSettings));
+    }
 
     // page title
-    eHead.appendChild (m_aPageTitle.convertToNode (aConversionSettings));
+    eHead.appendChild (this.m_aPageTitle.convertToNode (aConversionSettings));
 
     // base
-    eHead.appendChild (m_aBase.convertToNode (aConversionSettings));
+    eHead.appendChild (this.m_aBase.convertToNode (aConversionSettings));
 
     // links
     emitLinks (eHead, aConversionSettings);
@@ -441,7 +425,9 @@ public class HCHead extends AbstractHCElement <HCHead>
 
     // Ensure tag is not self-closed
     if (!eHead.hasChildren () && EHTMLElement.HEAD.mayNotBeSelfClosed ())
-      eHead.appendText ("");
+    {
+      eHead.appendText (""); //$NON-NLS-1$
+    }
   }
 
   @Override
@@ -449,20 +435,20 @@ public class HCHead extends AbstractHCElement <HCHead>
   public String getPlainText ()
   {
     // Use the page title as plain text
-    return m_aPageTitle.getPlainText ();
+    return this.m_aPageTitle.getPlainText ();
   }
 
   @Override
   public String toString ()
   {
     return ToStringGenerator.getDerived (super.toString ())
-                            .appendIfNotNull ("profile", m_sProfile)
-                            .append ("pageTitle", m_aPageTitle)
-                            .append ("base", m_aBase)
-                            .appendIfNotNull ("metaElements", m_aMetaElements)
-                            .appendIfNotNull ("links", m_aLinks)
-                            .appendIfNotNull ("CSS", m_aCSS)
-                            .appendIfNotNull ("JS", m_aJS)
+                            .appendIfNotNull ("profile", this.m_sProfile) //$NON-NLS-1$
+                            .append ("pageTitle", this.m_aPageTitle) //$NON-NLS-1$
+                            .append ("base", this.m_aBase) //$NON-NLS-1$
+                            .appendIfNotNull ("metaElements", this.m_aMetaElements) //$NON-NLS-1$
+                            .appendIfNotNull ("links", this.m_aLinks) //$NON-NLS-1$
+                            .appendIfNotNull ("CSS", this.m_aCSS) //$NON-NLS-1$
+                            .appendIfNotNull ("JS", this.m_aJS) //$NON-NLS-1$
                             .toString ();
   }
 }
